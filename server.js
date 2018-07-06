3379e45256bf4008b6beee50aebc0c81e50264b1
Joshua Payne <Intellijosh@MacBook-Pro.local>
2018-07-06 09:56:52 -0600

    mongoose connected

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = exp();
 const PORT = process.env.PORT || 3000;
 
 const mongoose = require('mongoose');
+const mongoUri =  process.env.MONGODB_URI || 'mongodb://localhost:27017/ffiction';
+
 
 app.get('/', (req, res) => {
   res.send('running');
@@ -10,4 +12,10 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, () => {
   console.log('listening on port ' + PORT);
+})
+
+mongoose.connect(mongoUri);
+const db = mongoose.connection;
+db.on('open', () => {
+  console.log('Connected to mongoose!');
 })
