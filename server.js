2c08b70974746d47f390abb86e3dd03dfca96cbb
Joshua Payne <Intellijosh@MacBook-Pro.local>
2018-07-06 09:27:56 -0600

    set to live

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const exp = require('express');
 const app = exp();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('running');
 })
 
-app.listen(port, () => {
-  console.log('listening on port ' + port);
+app.listen(PORT, () => {
+  console.log('listening on port ' + PORT);
 })
