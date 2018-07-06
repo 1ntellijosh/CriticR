12d80309bbef76d0e4e6a9aaa3287a7e1560bd6f
Joshua Payne <Intellijosh@MacBook-Pro.local>
2018-07-06 08:46:23 -0600

    route created

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log('hello');
+const exp = require('express');
+const app = exp();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('running');
+})
+
+app.listen(port, () => {
+  console.log('listening on port ' + port);
+})
